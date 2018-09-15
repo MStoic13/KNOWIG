4611fe7aacc57579e9a9df3be4e79ff7d405242a
Maria Stoica <34403668+MStoic13@users.noreply.github.com>
2018-09-15 08:40:32 -0700

    basic starting point

---
+++ data.js
@@ -0,0 +1,8 @@
+var nodes = [
+	{id: 1, label: 'PSYCHOLOGY', title: 'Subject', value: 50, group: 1, x: 0, y: 0},
+    {id: 2, label: 'Human Needs', title: 'Topic', value: 40, group: 1, x: 200, y: 10}
+];
+
+var edges = [
+	{from: 1, to: 2}
+];
